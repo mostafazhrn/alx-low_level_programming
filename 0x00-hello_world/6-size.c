aebc061c81acc83e3ea88d3a54187578cf1f4623
mostafzhrn <mostafzhrn@gmail.com>
2023-08-10 08:55:04 -0700

    sizesOfTypes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - this code run the size of various type
+ *Return: this program shall return 0 if success
+ */
+int main(void)
+{
+printf("size of a char: %d bytes \n", sizeof(char));
+printf("size of an int: %d bytes \n", sizeof(int));
+printf("size of a long int: %d bytes \n", sizeof(long int));
+printf("size of a long long int: %d bytes \n", sizeof(long long int));
+printf("size of a float int: %d bytes \n", sizeof(float));
+return (0);
+}
