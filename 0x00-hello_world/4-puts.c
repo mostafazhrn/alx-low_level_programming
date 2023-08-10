cbe8ad8948cbf9b731c87292fc45b3aff3d16732
mostafzhrn <mostafzhrn@gmail.com>
2023-08-10 08:00:42 -0700

    putsFunction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
+/**
+ * main: this simple code prints a text using 
+ * puts and shall return zero if success
+ */
 int main(void)
 {
-  puts("Programming is like building a multilingual puzzle, \n");
-  return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
