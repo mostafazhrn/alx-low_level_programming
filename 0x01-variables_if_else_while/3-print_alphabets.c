525e97fa163d75c69a052a6fc2b11a095b46cc6a
mostafzhrn <mostafzhrn@gmail.com>
2023-08-11 10:44:49 -0700

    alphabetLowerUpper

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *Description: main - this code prints A-Z two times
+ * one in lower case and one in upper
+ * Return: in sucess return 0
+ */
+int main(void)
+{
+char x;
+char y;
+for (x = 'a'; x <= 'z'; ++x)
+{
+putchar(x);
+}
+for (y = 'A'; y <= 'Z'; ++y)
+{
+putchar(y);
+}
+putchar('\n');
+return (0);
+}
