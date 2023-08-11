bd2d1404fed0c2207c62d01b58ad65214cd2da05
mostafzhrn <mostafzhrn@gmail.com>
2023-08-11 08:43:14 -0700

    README

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ *Description: main - this code print the last digit
+ *in variable n and use if stmt
+ *Return: in success it shall return 0
+ */
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* the code i added */
+
+return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+this a readme file for the task this project focus on using if else statement
