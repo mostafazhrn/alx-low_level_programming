6854c3c49069acdf06ea3a1f0d5edf2e39ea5908
mostafzhrn <mostafzhrn@gmail.com>
2023-08-15 15:30:22 -0700

    add98

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *add - this prog adds two integar
+ *@x: the 1st integar
+ *@y: the 2nd integar
+ *Return: it shall return the answer
+ */
+int add(int x, int y)
+{
+return (x + y);
+}
