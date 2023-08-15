fa8d398c32294335f908dbd1333264fbd49f75d2
mostafzhrn <mostafzhrn@gmail.com>
2023-08-15 14:00:40 -0700

    printLastDigits

--- 0x02-functions_nested_loops/7-main.c
+++ 0x02-functions_nested_loops/7-main.c
@@ -6,10 +6,8 @@
 int main(void)
 {
 int r;
-r = print_last_digit(98);
-_putchar('0' + r);
-r = print_last_digit(0);
-_putchar('0' + r);
+print_last_digit(98);
+print_last_digit(0);
 r = print_last_digit(-1024);
 _putchar('0' + r);
 _putchar('\n');
