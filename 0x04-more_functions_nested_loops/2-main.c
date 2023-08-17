be7e3f096da9d8107ebc92c622002eddbf2955d3
mostafzhrn <mostafzhrn@gmail.com>
2023-08-17 05:59:21 -0700

    multAB

---
+++ 0x04-more_functions_nested_loops/2-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - verify the code
+ *
+ * Return: in success it shall return 0.
+ */
+int main(void)
+{
+printf("%d\n", mul(98, 1024));
+printf("%d\n", mul(-402, 4096));
+return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *mul - this code multipy ints
+ *@a: the first int
+ *@b: the 2nd int
+ *Return: it shall retunr the mul of ab
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
