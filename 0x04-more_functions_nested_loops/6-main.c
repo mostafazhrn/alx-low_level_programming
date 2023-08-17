ddd94a024e43d9dbbdae5e950703a5f57b82f3d6
mostafzhrn <mostafzhrn@gmail.com>
2023-08-17 08:04:58 -0700

    drawLines

---
+++ 0x04-more_functions_nested_loops/6-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - verifys the code
+ *
+ * Return: in sucess return 0.
+ */
+int main(void)
+{
+print_line(0);
+print_line(2);
+print_line(10);
+print_line(-4);
+return (0);
+}
