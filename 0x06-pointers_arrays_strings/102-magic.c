132e1960fc35a16c1ffdaff940014c403cd99859
mostafzhrn <mostafzhrn@gmail.com>
2023-08-24 15:24:08 -0700

    magic98

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - i will make the magic happen by prt 98
+ *Return: it shall return 0 in success
+ */
+int main(void)
+{
+int n;
+int a[5];
+int *p;
+a[2] = 1024;
+p = &n;
+p[5] = 98;
+printf("a[2] = %d\n", a[2]);
+return (0);
+}
