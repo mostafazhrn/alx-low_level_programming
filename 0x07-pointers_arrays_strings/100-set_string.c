4a1c04f0ba40ff6e0d855ce74a9648581ec28131
mostafazhrn <mostafazhrn@gmail.com>
2023-08-28 10:01:26 -0400

    set2Str

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *set_string - this code directs a pointer to a char
+ *@s: this represents the value of char
+ *@to: this represents the 2nd int
+ *Return: it shall return the valu of ptr to char
+ */
+void set_string(char **s, char *to)
+{
+*s = to;
+}
