d6efb2592f97d756a1c04d33d56ec8b901030860
mostafazhrn <mostafazhrn@gmail.com>
2023-09-11 10:27:21 -0400

    printFileName

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main - this [rogram shall print the name of the its file
+ *Return: it shall retrun 0 in success
+ */
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
