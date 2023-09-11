70ce0f42d35c57660de79fef4016d7fac153e28f
mostafazhrn <mostafazhrn@gmail.com>
2023-09-11 11:21:19 -0400

    structDog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DG_H
+#define DG_H
+/**
+ *struct dog - this struct shall contain the dog name, age owner
+ *@name: this shall represent name of perro
+ *@age: this represent edad of dog
+ *@owner: this shall represent the owner of dogzie
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
