3be8f86dc599784e614700d71b400ff88a5ae60b
mostafazhrn <mostafazhrn@gmail.com>
2023-09-13 13:05:29 -0400

    shouldwork

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -16,11 +16,12 @@ op_t ops[] = {
 {"%", op_mod},
 {NULL, NULL}
 };
-int i;
-for (i = 0; i < 5; i++)
+int i = 0;
+while (i < 5)
 {
 if (s && s[0] == ops[i].op[0] && !s[1])
 return (ops[i].f);
+i++;
 }
 return (NULL);
 }
