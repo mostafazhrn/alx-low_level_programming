357ac3f7e9f82bf36f0f2e2be152f3990740e622
mostafazhrn <mostafazhrn@gmail.com>
2023-09-14 14:30:12 -0400

    print2Nos

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -9,23 +9,23 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-  va_list digit;
-  unsigned int x = 0;
-  if (separator == NULL)
-    {
-      separator = "";
-    }
-  va_start(digit, n);
-  while (x < n)
-    {
-      printf("%d", va_arg(digit, int));
-      if (n == x + 1)
-	{
-	  break;
-	}
-      printf("%s", separator);
-      x++;
-    }
-  printf("\n");
-  va_end(digit);
+va_list digit;
+unsigned int x = 0;
+if (separator == NULL)
+{
+separator = "";
+}
+va_start(digit, n);
+while (x < n)
+{
+printf("%d", va_arg(digit, int));
+if (n == x + 1)
+{
+break;
+}
+printf("%s", separator);
+x++;
+}
+printf("\n");
+va_end(digit);
 }
