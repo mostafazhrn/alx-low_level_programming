1d17a38109900af31d30939909bcb565a40f276f
mostafazhrn <mostafazhrn@gmail.com>
2023-11-09 07:31:19 -0500

    freeDaLstfix

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -6,11 +6,11 @@
  */
 void free_dlistint(dlistint_t *head)
 {
-  dlistint_t *x;
-  while (head)
-    {
-      x = head->next;
-      free(head);
-      head = x;
-    }
+dlistint_t *x;
+while (head)
+{
+x = head->next;
+free(head);
+head = x;
+}
 }
