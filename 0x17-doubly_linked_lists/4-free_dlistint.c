99d2694f671c964502837679ad1ba513728a0dbd
mostafazhrn <mostafazhrn@gmail.com>
2023-11-09 07:30:04 -0500

    freeDaLst

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ *free_dlistint - this code shall free the list
+ *@head: this shall represent the start of ptr
+ *Return: it shall return the freed list
+ */
+void free_dlistint(dlistint_t *head)
+{
+  dlistint_t *x;
+  while (head)
+    {
+      x = head->next;
+      free(head);
+      head = x;
+    }
+}
