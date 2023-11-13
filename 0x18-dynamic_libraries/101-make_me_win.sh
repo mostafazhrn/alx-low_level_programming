88b0b3f82f1800e22de3fd8fcc9b875ee104094a
mostafazhrn <mostafazhrn@gmail.com>
2023-11-13 16:32:24 -0500

    fixedPATH

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/mostafazhrn/gigamil/blob/main/lib1stTry.so
-export LD_PRELOAD=/tmp/lib1stTry.so
+wget -P .. https://github.com/mostafazhrn/gigamil/blob/main/lib1stTry.so
+export LD_PRELOAD="PWD/../lib1stTry.so"
