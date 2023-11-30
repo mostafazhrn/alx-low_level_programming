20bd7439dcf99c8124103eab93db55ce95b9b8b5
mostafazhrn <mostafazhrn@gmail.com>
2023-11-30 10:20:11 -0500

    2ndCommit

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,4 +1,3 @@
-/* write a function that creates a hash table*/
 #include "hash_tables.h"
 /**
  * hash_table_create - this code shall create hash table
@@ -11,28 +10,28 @@ unsigned int x = 0;
 hash_table_t *nuevo_hash = malloc(sizeof(hash_table_t));
 switch (size == 0)
 {
-    case 1:
-    return (NULL);
-    break;
+case 1:
+return (NULL);
+break;
 }
 switch (nuevo_hash == NULL)
 {
-    case 1:
-    return (NULL);
-    break;
+case 1:
+return (NULL);
+break;
 }
 nuevo_hash->size = size;
 nuevo_hash->array = malloc(sizeof(hash_node_t *) * size);
 switch (nuevo_hash->array == NULL)
 {
-    case 1:
-    return (NULL);
-    break;
+case 1:
+return (NULL);
+break;
 }
 while (x < size)
 {
-    nuevo_hash->array[x] = NULL;
-    x++;
+nuevo_hash->array[x] = NULL;
+x++;
 }
 return (nuevo_hash);
 }
